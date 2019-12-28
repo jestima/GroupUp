95fdf8a0d76b0ed73282901cd3aa1946e2c3b409
manuelbpc <55871964+manuelbpc@users.noreply.github.com>
2019-12-28 04:16:35 +0000

    Actually fixing this now

--- routes/discordRoute.js
+++ routes/discordRoute.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var discordRouter = express.Router();
-var discordDAO = require("../models/discordChat.js");
+var discordDAO = require("../models/discordChat");
 
 
 discordRouter.post("/CreateChannel", function (req, res, next) {
-    discordDAO.createChannel(req.body.eventName)
+    discordDAO.createChannel(
+        req.body.eventName,
+        function (result) {
+            res.send(result)
+        },
+        next
+    )
 });
 
 module.exports = discordRouter
