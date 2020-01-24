1996bb718b3b73317caefc03894dfaaa2ce249b5
manuelbpc <55871964+manuelbpc@users.noreply.github.com>
2020-01-24 15:36:04 +0000

    organized code

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -49,18 +49,6 @@ function verifyUser(username, mail) {
     }
 }
 
-document.getElementById("signinLink").addEventListener("click", switchForms)
-document.getElementById("signupLink").addEventListener("click", switchForms)
-
-function switchForms() {
-    if (document.getElementById("signin").style.display === "none") {
-        document.getElementById("signin").style.display = "block"
-        document.getElementById("signup").style.display = "none"
-    } else {
-        document.getElementById("signin").style.display = "none"
-        document.getElementById("signup").style.display = "block"
-    }
-}
 
 function login() {
     var mail = document.getElementById("your_name").value;
